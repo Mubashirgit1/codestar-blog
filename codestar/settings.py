477d4dd4ae34cdd4d953d1fb20425303241f14e0
Mubashirgit1 <mubashir.connect@gmail.com>
2025-10-04 16:36:48 +0100

    update alowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--=%c7d80y+u6xrul@zv(gplf)8yaa(_e*uambk$$t%ry#f2!ri'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    '.herokuapp.com'
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',]
 
 
 # Application definition
