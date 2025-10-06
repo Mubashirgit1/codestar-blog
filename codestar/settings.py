57b20eca866fd1fc63a48f4944748b3ced8b496b
Mubashirgit1 <mubashir.connect@gmail.com>
2025-10-06 21:16:52 +0100

    added comment CRUD

--- codestar/settings.py
+++ codestar/settings.py
@@ -36,7 +36,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
